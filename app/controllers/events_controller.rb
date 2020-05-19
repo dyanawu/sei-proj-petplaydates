bcd5f3a7ecab702ddcef7108189a639b6d649a57
wickker <choydianchun@gmail.com>
2020-05-19 16:39:10 +0800

    Tweaking date-time form display

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :description, :capacity, :location, :start_time, :end_time, :min_pets)
+      params.require(:event).permit(:title, :description, :capacity, :location, :start_time, :end_time, :min_pets, :pet_ids => [])
     end
 end
