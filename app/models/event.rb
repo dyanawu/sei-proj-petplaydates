b4cec80f1a659dba84689779fdb796a292786910
chelsejw <chelsejw@gmail.com>
2020-05-20 19:52:44 +0800

    add validations in html and model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ApplicationRecord
   has_and_belongs_to_many :pets
   belongs_to :user
-
+  
+  
   def status
     if self.start_time < Time.now
       "past"
@@ -12,4 +13,15 @@ class Event < ApplicationRecord
     end
   end
 
+  validate :end_after_start
+  validates :start_time, :end_time, :presence => true
+
+  private
+    def end_after_start
+      return if end_time.blank? || start_time.blank?
+    
+      if end_time < start_time + 30.minutes
+        errors.add(:end_time, "must be at least 30 mins after the start time") 
+      end 
+    end
 end
