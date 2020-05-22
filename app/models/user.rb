40ee067bdf247eebb686f266f2f353a545ef7d82
Dyana Wu <dyanawu@gmail.com>
2020-05-22 15:37:15 +0800

    Delete associated profile/pets/events on user delete account

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  has_one :profile
-  has_many :pets
-  has_many :events
+  has_one :profile, dependent: :destroy
+  has_many :pets, dependent: :destroy
+  has_many :events, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, authentication_keys: [:login]
