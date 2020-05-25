fe93553e17dd647c41548bf9e2690b446fcff49b
Dyana Wu <dyanawu@gmail.com>
2020-05-25 09:38:44 +0800

    Refactor date display
    - remove unusued datetime display methods
    - replace strf strings in views with updated datetime display methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -33,27 +33,23 @@ require 'action_view'
   end
 
   def start_datetime
-    self.start_time.getlocal.strftime("%d %B %Y, %I:%M %p")
-  end
-
-  def end_datetime
-    self.end_time.getlocal.strftime("%d %B %Y, %I:%M %p")
+    self.start_time.localtime.strftime("%d %B, %I:%M %p")
   end
 
   def start_date_str
-    self.start_time.getlocal.strftime("%d %B %Y")
+    self.start_time.localtime.strftime("%d %B %Y")
   end
 
   def start_time_str
-    self.start_time.getlocal.strftime("%I:%M %p")
+    self.start_time.localtime.strftime("%I:%M %p")
   end
 
   def end_date_str
-    self.end_time.getlocal.strftime("%d %B %Y")
+    self.end_time.localtime.strftime("%d %B %Y")
   end
 
   def end_time_str
-    self.end_time.getlocal.strftime("%I:%M %p")
+    self.end_time.localtime.strftime("%I:%M %p")
   end
 
   def self.today
