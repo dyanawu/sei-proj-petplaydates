537541bd3b989d51da1b203a5c9f23007b916e4e
Dyana Wu <dyanawu@gmail.com>
2020-05-26 14:36:25 +0800

    Resolve merge conflict from master

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -82,7 +82,7 @@ class EventsController < ApplicationController
     @event = parse_event_to_local_time(Event.new(event_params))
     @event.user = current_user
 
-    if !params[:event][:img_url].nil?
+    if params[:event][:img_url] != ""
       uploaded_file = params[:event][:img_url].path
       cloudinary_file = Cloudinary::Uploader.upload(uploaded_file)
       @event.img_url = cloudinary_file['url']
@@ -104,7 +104,8 @@ class EventsController < ApplicationController
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
   def update
-    if !params[:event][:img_url].nil?
+    @types = Type.all
+    if params[:event][:img_url] != ""
       uploaded_file = params[:event][:img_url].path
       cloudinary_file = Cloudinary::Uploader.upload(uploaded_file)
       @event.img_url = cloudinary_file['url']
